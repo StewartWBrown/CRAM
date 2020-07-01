c062ddee726779ae051caae3db36c495e0f74799
Jacosdin9 <2314964o@student.gla.ac.uk>
2020-07-01 20:45:26 +0100

    Working backend on Android Studio

---
+++ app/src/main/java/com/example/cram_project/Subject.java
@@ -0,0 +1,36 @@
+package com.example.cram_project;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Subject {
+
+    String name;
+    int workloads;
+    ArrayList<Integer> workCompleted;
+    Date startDate;
+    Date endDate;
+    Date examDate;
+    ArrayList<Integer> remainingWork;
+
+    public Subject(String _name, int _workloads, ArrayList<Integer> _workCompleted, Date _startDate, Date _endDate, Date _examDate) {
+        this.name = _name;
+        this.workloads = _workloads;
+        this.workCompleted = _workCompleted;
+        this.startDate = _startDate;
+        this.endDate = _endDate;
+        this.examDate = _examDate;
+        this.remainingWork = find_workload();
+    }
+
+    public ArrayList<Integer> find_workload() {
+        ArrayList<Integer> workLoad = new ArrayList<>();
+        for(int i=1; i<workloads+1; i++) {
+            if(!workCompleted.contains(i)) {
+                workLoad.add(i);
+            }
+        }
+        return workLoad;
+    }
+
+}
