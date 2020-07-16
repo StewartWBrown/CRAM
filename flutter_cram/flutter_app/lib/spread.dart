c2eae39fb083df4b6a2d42dbe38e2e574c6c6360
Jacob Osdin <2314964o@student.gla.ac.uk>
2020-07-16 14:15:33 +0100

    spread finished

--- flutter_cram/flutter_app/lib/spread.dart
+++ flutter_cram/flutter_app/lib/spread.dart
@@ -15,7 +15,7 @@ class Spread {
     }
 
     //initially, spread workloads for subject between remaining study dates given
-    Map<DateTime, Map<String, List<Workload>>> calendar = Map();
+    Map<DateTime, Map<String, List<Workload>>> calendar = SplayTreeMap();
     for (Subject subject in subjects){
       int totalDays = subject.endDate.difference(subject.startDate).inDays;
 
@@ -53,8 +53,56 @@ class Spread {
       }
     }
 
-    Map<DateTime, Map<String, List<Workload>>> sortedMap = SplayTreeMap.from(calendar);
+    //secondary spreading - taking difficulty into account
 
+    int prevDifficulty = 0;
+    DateTime prevDate = null;
+    for(DateTime date in calendar.keys){
+      int currentDifficulty = 0;
+      for(String subject in calendar[date].keys){
+        for(Workload wl in calendar[date][subject]){
+          currentDifficulty += wl.difficulty;
+        }
+      }
+
+      //if current date is weighted 2 or more in difficulty than previous date, use wlToMove method to decide which
+      //workloads in the current date to move back to previous date
+      if(currentDifficulty-prevDifficulty>1 && prevDate!=null){
+        Map<String, List<Workload>> wlToMove = findWlToMove(calendar[date], ((currentDifficulty-prevDifficulty)/2).floor(), prevDate, subjects);
+
+        for(String subj in wlToMove.keys){
+          for(Workload wl in wlToMove[subj]){
+            calendar[prevDate].putIfAbsent(subj, () => List<Workload>());
+            calendar[prevDate][subj].add(wl);
+            calendar[date][subj].remove(wl);
+            if(calendar[date][subj].isEmpty){
+              calendar[date].remove(subj);
+            }
+            prevDifficulty += wl.difficulty;
+            currentDifficulty -= wl.difficulty;
+          }
+        }
+      }
+
+      if(prevDifficulty-currentDifficulty>1){
+        Map<String, List<Workload>> wlToMove = findWlToMove(calendar[prevDate], ((prevDifficulty-currentDifficulty)/2).floor(), date, subjects);
+
+        for(String subj in wlToMove.keys){
+          for(Workload wl in wlToMove[subj]){
+            calendar[date].putIfAbsent(subj, () => List<Workload>());
+            calendar[date][subj].add(wl);
+            calendar[prevDate][subj].remove(wl);
+            if(calendar[prevDate][subj].isEmpty){
+              calendar[prevDate].remove(subj);
+            }
+            currentDifficulty += wl.difficulty;
+            prevDifficulty -= wl.difficulty;
+          }
+        }
+      }
+      prevDifficulty = currentDifficulty;
+      prevDate = date;
+    }
 
 //    PRETTY PRINT
 //    for(DateTime date in calendar.keys){
@@ -71,6 +119,46 @@ class Spread {
 //      print("");
 //    }
     return calendar;
+  }
+
+  Map<String, List<Workload>> findWlToMove(Map<String, List<Workload>> workload, int difficulty, DateTime currentDate, List<Subject> subjects){
+    Map<String, List<Workload>> wlToMove = Map();
+    List<Workload> finalWl;
+    int currentDifficultyTotal = 0;
+
+    for(String subject in workload.keys){
+      //check if current subject being checked's endDate comes AFTER the date workloads are being moved to
+      bool beforeEndAndAfterStart = false;
+      for(Subject s in subjects){
+        if(s.name == subject){
+          if((s.endDate == currentDate || s.endDate.isAfter(currentDate)) && (s.startDate == currentDate || s.startDate.isBefore(currentDate))){
+            beforeEndAndAfterStart = true;
+            break;
+          }
+        }
+      }
+      if(beforeEndAndAfterStart){
+        //find the correct workloads to move forward
+        for(Workload wl in workload[subject]){
+          //if this workload is the exact difficulty weighting we're looking for, return that workload and we're done
+          if(wl.difficulty == difficulty){
+            finalWl = [wl];
+            wlToMove = Map();
+            wlToMove[subject] = finalWl;
+            return wlToMove;
+          } else if(wl.difficulty < difficulty && currentDifficultyTotal < difficulty - wl.difficulty){
+            if(wlToMove.containsKey(subject)){
+              wlToMove[subject].add(wl);
+            } else{
+              finalWl = [wl];
+              wlToMove[subject] = finalWl;
+            }
+            currentDifficultyTotal += wl.difficulty;
+          }
+        }
+      }
+    }
+    return wlToMove;
   }
 
 }
