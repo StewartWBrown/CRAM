0621b6f51663539fde924fdfda6798d26fb170bb
Jacob Osdin <2314964o@student.gla.ac.uk>
2020-07-17 22:18:48 +0100

    DORIME

--- flutter_cram/flutter_app/lib/mainScreen.dart
+++ flutter_cram/flutter_app/lib/mainScreen.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/spread.dart';
 import 'home_page_body.dart';
 import 'model/subject.dart';
+import 'model/workload.dart';
 import 'spread.dart';
+import 'calendar.dart';
 
 
 class MainScreen extends StatelessWidget {
+  Map<DateTime, Map<String, List<Workload>>>calendar1 = Spread().spread(subjects, List());
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,13 +36,12 @@ class MainScreen extends StatelessWidget {
               body: new TabBarView(
                 children: <Widget>[
                   new HomePageBody(),
-                  new HomePageBody(),
+                  new Calendar(),
                 ],
               ),
             ),
             floatingActionButton: FloatingActionButton.extended(
               onPressed: () {
-                Spread().spread(subjects, List());
               },
               label: Text('Add Subject'),
               icon: Icon(Icons.add),
