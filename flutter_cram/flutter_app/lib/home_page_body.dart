adc9bf77b772e0176a8029c81dfe06e63ed087a5
Stewart <2313225b@student.gla.ac.uk>
2020-07-23 13:20:14 +0100

    Case instructions

--- flutter_cram/flutter_app/lib/home_page_body.dart
+++ flutter_cram/flutter_app/lib/home_page_body.dart
@@ -10,47 +10,51 @@ class HomePageBody extends StatefulWidget {
 class _HomePageBodyState extends State<HomePageBody> {
   Future<List<Subject>> subjects = updateList();
   @override
+  // ignore: missing_return
   Widget build(BuildContext context) {
     FutureBuilder<List<Subject>>(
         future: subjects,
         builder: (context, snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return Text("YUCK");
-          }
-          if (snapshot.hasError) {
-            return Text("ERROR");
-          }
-          List<Subject> subjects = snapshot.data ?? [];
-          if (subjects.isEmpty) {
-            return Text("Enter a subject bitch");
-          }
-          return Row(
-              children: [
-                Expanded(
-                  child: new Container(
-                    color: new Color(0xFF0c6f96),
-                    child: new CustomScrollView(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: false,
-                      slivers: <Widget>[
-                        new SliverPadding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 24.0),
-                          sliver: new SliverList(
-                            delegate: new SliverChildBuilderDelegate(
-                                  (context, index) =>
-                              new SubjectRow(subjects[index]),
-                              childCount: subjects.length,
-                            ),
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                  child: CircularProgressIndicator()
+              );
+            default:
+              List<Subject> subjects = snapshot.data ?? [];
+              if (subjects.isEmpty) {
+                return Text("Enter a subject bitch");
+              }
+              else {
+                return Row(
+                    children: [
+                      Expanded(
+                        child: new Container(
+                          color: new Color(0xFF0c6f96),
+                          child: new CustomScrollView(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: false,
+                            slivers: <Widget>[
+                              new SliverPadding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 24.0),
+                                sliver: new SliverList(
+                                  delegate: new SliverChildBuilderDelegate(
+                                        (context, index) =>
+                                    new SubjectRow(subjects[index]),
+                                    childCount: subjects.length,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                )
-              ]);
+                      )
+                    ]);
+              }
+          }
         }
-
     );
   }
 }
